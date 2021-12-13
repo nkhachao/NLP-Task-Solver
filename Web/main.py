06beb2b9a94212a9d6d6813d2edb4e7eb445e8dc
Nguyễn Khắc Hào <nkhachao@gmail.com>
2021-12-13 18:01:40 +0700

    Moved question answering code from web app to model.py

--- Web/main.py
+++ Web/main.py
@@ -1,24 +1,11 @@
 from re import template
 import re
 from flask import Flask, render_template, request, redirect, url_for
-import mysql.connector
 import datetime
-from transformers import pipeline
+from model import answer
 
 app = Flask(__name__, template_folder='template')
 
-# passage = 'The iPhone is a line of smartphones designed and marketed by Apple Inc. that use Apple\'s iOS mobile operating system. The first-generation iPhone was announced by then-Apple CEO Steve Jobs on January 9, 2007. Since then, Apple has annually released new iPhone models and iOS updates. As of November 1, 2018, more than 2.2 billion iPhones had been sold.'
-# question = 'When was the first-generation iPhone announced?'
-
-qa_pipeline = pipeline(
-    "question-answering",
-    model="csarron/bert-base-uncased-squad-v1",
-    tokenizer="csarron/bert-base-uncased-squad-v1"
-)
-
-def answer(passage, question):
-    predictions = qa_pipeline({'context': passage, 'question': question})
-    return predictions['answer']
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
